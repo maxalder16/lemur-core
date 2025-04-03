3a0e747df04730e4a676f02379acdd0f618a4658
Max Alderson <max@weareyourbridge.com>
2025-04-03 12:04:59 +0100

    feat(main): eslint setup

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginVue from 'eslint-plugin-vue';
+import prettierConfig from 'eslint-config-prettier';
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs['flat/essential'],
+  {
+    files: ['**/*.vue'],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  prettierConfig,
+]);
