bd42c5d11a1b8cb017e6dc8708d8f55bdb442fff
Max Alderson <max@weareyourbridge.com>
2025-04-03 13:48:22 +0100

    feat(main): web history

--- src/providers/router.ts
+++ src/providers/router.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import { routes } from '../pages';
 
 export const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes,
 });
