916bf91bbf2a56a3fda5fae72b23f0f3f3fdcc19
Max Alderson <max@weareyourbridge.com>
2025-04-03 15:04:48 +0100

    feat(user): setting up auth0 config

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,18 @@ import { createApp } from 'vue';
 import './style.css';
 import App from './app/App.vue';
 import { router } from './providers';
+import { createAuth0 } from '@auth0/auth0-vue';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(
+  createAuth0({
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+    authorizationParams: {
+      redirect_uri: import.meta.env.VITE_AUTH0_CALLBACK_URL,
+    },
+  })
+);
+
+app.use(router).mount('#app');
--- src/widgets/navigation/Header.vue
+++ src/widgets/navigation/Header.vue
@@ -1,6 +1,26 @@
+<script setup lang="ts">
+  import { useAuth0 } from '@auth0/auth0-vue';
+
+  const { loginWithRedirect } = useAuth0();
+
+  function handleLogin() {
+    loginWithRedirect({
+      appState: {
+        target: '/',
+      },
+    });
+  }
+</script>
+
 <template>
-  <div class="flex gap-3 pb-3">
+  <div class="flex items-center gap-8 pb-3">
     <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
     <RouterLink :to="{ name: 'Tools' }">Tools</RouterLink>
+    <div
+      class="cursor-pointer rounded-sm bg-blue p-3 text-white"
+      @click="handleLogin()"
+    >
+      Login
+    </div>
   </div>
 </template>
