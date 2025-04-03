314f1cf74cdac979c934e51419808663e0304cc2
Max Alderson <max@weareyourbridge.com>
2025-04-03 10:57:04 +0100

    feat(main): init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  type Props = {
+    message: string;
+  }
+
+  defineProps<Props>()
+
+  const count = ref(0)
+</script>
+
+<template>
+  <h1 class="font-bold">{{ message }}</h1>
+</template>
