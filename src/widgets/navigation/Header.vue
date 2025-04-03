f5002ad53fe4cb073a2f2c28476f0e6a91856174
Max Alderson <max@weareyourbridge.com>
2025-04-03 16:37:03 +0100

    feat(user): themeing

--- src/widgets/navigation/Header.vue
+++ src/widgets/navigation/Header.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
   import { useAuth0 } from '@auth0/auth0-vue';
 
-  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+  const { loginWithRedirect, logout, isAuthenticated, user } = useAuth0();
 
   function handleLogin(signup?: boolean) {
     loginWithRedirect({
@@ -30,21 +30,27 @@
     <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
     <RouterLink :to="{ name: 'Tools' }">Tools</RouterLink>
     <div
-      class="cursor-pointer rounded-sm bg-blue p-3 text-white"
+      class="cursor-pointer rounded-sm bg-primary px-3.5 py-2.5 font-bold text-white transition-all hover:bg-primary-400"
       @click="handleLogin()"
       v-if="!isAuthenticated"
     >
       Login
     </div>
     <div
-      class="cursor-pointer rounded-sm bg-blue p-3 text-white"
+      class="cursor-pointer rounded-sm bg-primary px-3.5 py-2.5 font-bold text-white transition-all hover:bg-primary-400"
       @click="handleLogin(true)"
       v-if="!isAuthenticated"
     >
       Sign up
     </div>
+    <RouterLink
+      v-if="isAuthenticated && user?.picture"
+      :to="{ name: 'UserProfile' }"
+    >
+      <img :src="user.picture" class="size-[50px] rounded-full" />
+    </RouterLink>
     <div
-      class="cursor-pointer rounded-sm bg-blue p-3 text-white"
+      class="cursor-pointer rounded-sm bg-primary px-3.5 py-2.5 font-bold text-white transition-all hover:bg-primary-400"
       @click="handleLogout()"
       v-if="isAuthenticated"
     >
