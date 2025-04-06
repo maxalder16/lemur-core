9cf4ca685b51c7ccaf151b3ab55e6b5bee9281df
Max Alderson <maxalder16@googlemail.com>
2025-04-06 16:26:23 +0100

    feat(app): remove dead store

--- src/widgets/navigation/Header.vue
+++ src/widgets/navigation/Header.vue
@@ -1,16 +1,10 @@
 <script setup lang="ts">
-  import { useAdminStore } from '../../entities/admin/model';
   import UserNav from './UserNav.vue';
-  import { storeToRefs } from 'pinia';
-
-  const adminStore = useAdminStore();
-  const { state } = storeToRefs(adminStore);
 </script>
 
 <template>
   <div class="flex items-center bg-secondary p-5">
     <div class="flex flex-1 items-center gap-8 px-5">
-      {{ state }}
       <RouterLink :to="{ name: 'Home' }">Home</RouterLink>
       <RouterLink :to="{ name: 'Tools' }">Tools</RouterLink>
     </div>
