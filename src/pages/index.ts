5e77de3cf3d0c3af0bd2d22badd8f32f926d2090
Max Alderson <maxalder16@googlemail.com>
2025-04-06 16:24:14 +0100

    feat(app): remove auth references

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,6 +1,5 @@
 import HomeView from './Index.vue';
 import ToolsView from './Tools.vue';
-import ProfileView from './user/Profile.vue';
 
 const routes = [
   {
@@ -12,16 +11,6 @@ const routes = [
     path: '/tools',
     name: 'Tools',
     component: ToolsView,
-  },
-  {
-    path: '/callback',
-    name: 'UserCallback',
-    component: ProfileView,
-  },
-  {
-    path: '/user/profile',
-    name: 'UserProfile',
-    component: ProfileView,
   },
 ];
 
